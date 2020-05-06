de4ea3bb086b70431d493a947d79b619e1c5f37a
afwjy16 <afif0071997@gmai.com>
2020-05-06 16:25:55 +0700

    update element star belum

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,15 +2,26 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
 //import json data={nama_json.nama data yang akan digunakan pada json contoh landingPage.hero}
 import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
   render() {
     return (
       <>
         <Header {...this.props}> </Header>
-        <Hero data={landingPage.hero} />
-        <MostPicked data={landingPage.mostPicked} />
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
+        <Testimony></Testimony>
       </>
     );
   }
