ad24ca17514e83fc732df757cb33c89e87fdbccf
afwjy16 <afif0071997@gmai.com>
2020-05-06 07:38:29 +0700

    update hero and mostpicked

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -87,4 +87,5 @@ Button.propTypes = {
   isBlock: propTypes.bool,
   isExternal: propTypes.bool,
   hasShadow: propTypes.bool,
+  isPrimary: propTypes.bool,
 };
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import Header from "parts/Header";
-
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+//import json data={nama_json.nama data yang akan digunakan pada json contoh landingPage.hero}
+import landingPage from "json/landingPage.json";
 export default class LandingPage extends Component {
   render() {
     return (
       <>
-        <Header {...this.props}></Header>
+        <Header {...this.props}> </Header>
+        <Hero data={landingPage.hero} />
+        <MostPicked data={landingPage.mostPicked} />
       </>
     );
   }
